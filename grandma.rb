76c08b65ff231718089d4566a7b54a4dc390b6de
Anika Christopher <anika.mchristopher@gmail.com>
2019-10-13 03:05:00 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,18 +1,18 @@
 speak_to_grandma = "Hello Grandma"
 
- if "Hello Grandma" == "Hi"
+ if speak_to_grandma == "Hi"
    puts "HUH?! SPEAK UP, SONNY!"
    
- elsif "Hello Grandma" == "Hi Nana, how are you?"
+ elsif speak_to_grandma == "Hi Nana, how are you?"
     puts "HUH?! SPEAK UP, SONNY!"
     
- elsif "Hello Grandma" == "WHAT DID YOU EAT TODAY?"
+ elsif speak_to_grandma == "WHAT DID YOU EAT TODAY?"
     puts "NO, NOT SINCE 1938!"
     
- elsif "Hello Grandma" == "WHAT?"
+ elsif speak_to_grandma == "WHAT?"
     puts "NO, NOT SINCE 1938!"
     
- else "Hello Grandma" == "I LOVE YOU GRANDMA!"
+ else speak_to_grandma == "I LOVE YOU GRANDMA!"
     puts "I LOVE YOU TOO PUMPKIN!"
       
  end
